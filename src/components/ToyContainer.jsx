65a47c7ca49c279b698f54c78549ac7debddc34d
timothydarrell14-glitch <timothydarrell14@gmail.com>
2026-05-10 14:21:33 +0300

    refactor: update ToyCard to display toy details and enhance ToyContainer with data fetching

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+
+  const [toyList, setToyList] = useState([])
+
+useEffect(() => {
+  const getData = async () => {
+    try{
+      let response = await fetch(`http://localhost:3001/toys`)
+
+      if(!response.ok){
+        throw new Error ("Network error")
+      }
+      let data = await response.json()
+      
+      setToyList(data)
+
+    }catch(error){
+      console.error("Error getting toys", error)
+    }
+  }
+}, [])
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {toyList.map((toy, index) => (
+        <ToyCard id={toy.id} key={index} name={toy.name} image={toy.image} likes={toy.likes}/>
+      ))}
+    </div>
   );
 }
 
